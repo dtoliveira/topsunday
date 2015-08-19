cad9c3f74bafc6b6fe8e757ebc9a4a8a8bdc8e4b
ElMatador <david.tavares.oliveira@gmail.com>
2015-08-19 02:15:06 +0100

    first commit

---
+++ TopSunday/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TopSunday.Models
+{
+    public class Player
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public int GamesCounter { get; set; }
+    }
+}
---
+++ TopSunday/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TopSunday.Startup))]
+namespace TopSunday
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
